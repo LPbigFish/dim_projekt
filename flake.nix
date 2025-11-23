c3a87c6e3a428d1d80ecd76abd67899a8a557e70
LPbigFish <lpbyblock@gmail.com>
2025-11-23 17:42:28 +0000

    DIM-Semestral

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A reproducible development environment for LaTeX and Typst";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "latex-typst-env";
+
+          buildInputs = with pkgs; [
+            # --- LaTeX Tools ---
+            # scheme-full includes essentially everything. 
+            # If disk space is an issue, switch to 'scheme-medium' or 'scheme-small'
+            texlive.combined.scheme-full
+            
+            # Additional LaTeX utilities (often included in scheme-full, but explicit is safe)
+            biber
+
+            # --- Typst Tools ---
+            typst       # The core compiler
+            typstyle    # Code formatter for Typst
+          ];
+        };
+      }
+    );
+}
